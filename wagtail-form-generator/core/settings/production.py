68c15a201146d532baee0d2b97ed6c82e5a59c8c
Forrest Herman <58410716+forrest-herman@users.noreply.github.com>
2021-09-23 13:11:22 -0400

    Update production.py

--- wagtail-form-generator/core/settings/production.py
+++ wagtail-form-generator/core/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # import dj_database_url
 
 env = os.environ.copy()
-# SECRET_KEY = env["SECRET_KEY"]
+SECRET_KEY = env["SECRET_KEY"]
 
 # DATABASES["default"] = dj_database_url.config()
 
