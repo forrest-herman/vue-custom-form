5d0b90dd3f672a82402e4a920d7ddef337075da4
Forrest Herman <58410716+forrest-herman@users.noreply.github.com>
2021-09-23 01:08:59 -0400

    app re-organizing

--- wagtail-form-generator/intake/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class IntakeConfig(AppConfig):
-    name = 'intake'
--- wagtail-form-generator/records/tasks.py
+++
@@ -1,5 +0,0 @@
-from celery import shared_task
-
-@shared_task
-def create_pdf(data):
-    print('TEST')
