f92cb425aba2540b3ac0dcae1a27bbb21e14504a
Forrest Herman <58410716+forrest-herman@users.noreply.github.com>
2021-09-23 13:24:12 -0400

    production update

--- wagtail-form-generator/core/settings/production.py
+++ wagtail-form-generator/core/settings/production.py
@@ -1,12 +1,19 @@
 from .base import *
+import os
 
-# import dj_database_url
+import dj_database_url
 
 env = os.environ.copy()
 SECRET_KEY = env["SECRET_KEY"]
 
-# DATABASES["default"] = dj_database_url.config()
+DATABASES["default"] = dj_database_url.config()
 
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ["*"]
 
 DEBUG = True
 
