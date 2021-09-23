f1dddd47ed07f833d553e5c82c7b5d8d105b85cf
Forrest Herman <58410716+forrest-herman@users.noreply.github.com>
2021-09-23 04:04:55 -0400

    production revert

--- wagtail-form-generator/core/settings/production.py
+++ wagtail-form-generator/core/settings/production.py
@@ -1,9 +1,9 @@
 from .base import *
 
-# import dj_database_url
+import dj_database_url
 
 
-# DATABASES["default"] = dj_database_url.config()
+DATABASES["default"] = dj_database_url.config()
 
 
 DEBUG = True
